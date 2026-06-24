cfba2fed0714f16e698ede07fd6c60ba135d3bbb
Ahmed Khan <aik2004umbc@gmail.com>
2026-06-24 01:01:26 -0500

    Updates initial files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Excel Duplicate Checker</h1>
+      <p>Upload an Excel file to find duplicate records.</p>
+    </div>
+  );
+}
+
+export default App;
